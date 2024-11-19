7cfa89357d98b0bfa88af71ce3e38103e9ebe348
DavyddBF <davydextreme460@gmail.com>
2024-11-19 11:20:55 -0300

    Atualizado

---
+++ Lógica e Algoritmos/Aula 2/aula-pratica/index.py
@@ -0,0 +1,19 @@
+# Escreva as seguintes expressões algébricas em linguagem Python:
+# a) O somatório dos 5 primeiros números inteiros e positivos
+
+print(1 + 2 + 3 + 4 + 5)
+
+# b) A média entre 23, 19 e 31
+
+media = (23 + 19 + 31) / 3
+print(media)
+
+# c) O número de vezes que 73 cabem em 403
+
+cabe = 403 // 73
+print(f'O número 73 cabe {cabe} vezes em 403')
+
+# d) A sobra quanto 403 é dividido por 73
+
+sobra = 403 % 73
+print(f'A sobra é {sobra}')
